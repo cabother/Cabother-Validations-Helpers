3da67f8fd59019e1233680719bd21b6ad9a5b567
Renato Fernandes Soares <renato.f.soares@hotmail.com>
2021-04-04 17:47:53 -0300

    feat: added test project

---
+++ test/Cabother.Validations.Helpers.Test/ValidationTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Cabother.Validations.Helpers.Test
+{
+    public class ValidationTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
